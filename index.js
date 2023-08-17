aa7d656d77df44501b9e89376f420f76c0ffb92e
Mahima137 <mahimagupta167@gmail.com>
2023-08-17 12:22:51 +0530

    Writing and reading from a file --> Project : Store data on screen

--- index.js
+++ index.js
@@ -14,27 +14,86 @@
 // server.listen(3000);
 
 
+
+
+
+
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//   const url = req.url;
+
+//   if (url === '/home') {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.end('Welcome home');
+//   } else if (url === '/about') {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.end('Welcome to About Us page');
+//   } else if (url === '/node') {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.end('Welcome to my Node.js project');
+//   } else {
+//     res.writeHead(404, { 'Content-Type': 'text/html' });
+//     res.end('Page not found');
+//   }
+// });
+
+// server.listen(3000);
+
+
+
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
-
-  if (url === '/home') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Welcome home');
-  } else if (url === '/about') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Welcome to About Us page');
-  } else if (url === '/node') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Welcome to my Node.js project');
+  const method = req.method;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    
+    fs.readFile('message.txt', 'utf8', (err, data) => {
+      if (!err) {
+        const messages = data.split('\n').filter(msg => msg.trim() !== '');
+        res.write('<body>');
+        res.write('<h1>Messages</h1>');
+        res.write('<ul>');
+        messages.forEach(message => {
+          res.write(`<li>${message}</li>`);
+        });
+        res.write('</ul>');
+        res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>');
+        res.write('</body>');
+        res.write('</html>');
+        return res.end();
+      }
+    });
+  } else if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', chunk => {
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      
+      fs.appendFile('message.txt', message + '\n', err => {
+        if (!err) {
+          res.writeHead(302, { 'Location': '/' });
+          return res.end();
+        }
+      });
+    });
   } else {
-    res.writeHead(404, { 'Content-Type': 'text/html' });
-    res.end('Page not found');
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
   }
 });
 
 server.listen(3000);
-
 
 
