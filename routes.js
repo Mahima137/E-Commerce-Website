34d4e8489a0de7d8fe314e596d653a708a40ea5f
Mahima137 <mahimagupta167@gmail.com>
2023-08-17 18:31:05 +0530

    How to install Nodemon and Package JSON. : Done

--- routes.js
+++ routes.js
@@ -13,6 +13,7 @@ const requestHandler = (req, res) => {
     return res.end();
   }
   
+  
   if (url === '/message' && method === 'POST') {
     const body = [];
     req.on('data', chunk => {
@@ -31,7 +32,7 @@ const requestHandler = (req, res) => {
   }
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
-  res.write('<head><title>My First Page</title><head>');
+  res.write('<head><title>My First Page</title></head>');
   res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
   res.write('</html>');
   res.end();
