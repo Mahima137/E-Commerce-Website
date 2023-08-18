51f000ffb1eb94796f2a0b1a848b61b1658070eb
Mahima137 <mahimagupta167@gmail.com>
2023-08-18 19:34:20 +0530

    Body parser : Done

--- index.js
+++ index.js
@@ -42,24 +42,30 @@
 
 
 
-const http = require('http');
+//const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); // Allows the request to continue to the next middleware in line
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product',(req, res, next) => {   // due to top to bottom approach we get this 1st
+    res.send('<form action="/product" method="POST"><input type="text" name ="title"><button type="submit">Add Item</button></form>');
 });
 
-app.use((req, res, next) => {
-    console.log('In another middleware!');
+app.use('/',(req, res, next) => {         // '/' every route by default start with it
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req, res, next) => {
     res.send('<h1>Hello from Express!</h1>');
 });
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-server.listen(3000);
+app.listen(3000);
 
 
