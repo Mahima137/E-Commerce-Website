312a71c68229610e031be5c76a44351520976bef
Mahima137 <mahimagupta167@gmail.com>
2023-08-18 20:24:44 +0530

    use routers, adding 404 error page, filtering path

--- index.js
+++ index.js
@@ -1,68 +1,18 @@
-// const http = require('http');
-
-// const server = http.createServer((req, res) =>{
-//     console.log(req.url, req.method, req.headers);
-//     //process.exit();
-//     res.setHeader('Content-Type', 'text/html');
-//     res.write('<html>');
-//     res.write('<head><title> My 1st HTML Code</title></head>');
-//     res.write('<body>Hello from my Node.js Server</body>');
-//     res.write('</html>');
-//     res.end();
-// });
-
-// server.listen(3000);
-
-
-
-
-
-
-// const http = require('http');
-
-// const server = http.createServer((req, res) => {
-//   const url = req.url;
-
-//   if (url === '/home') {
-//     res.writeHead(200, { 'Content-Type': 'text/html' });
-//     res.end('Welcome home');
-//   } else if (url === '/about') {
-//     res.writeHead(200, { 'Content-Type': 'text/html' });
-//     res.end('Welcome to About Us page');
-//   } else if (url === '/node') {
-//     res.writeHead(200, { 'Content-Type': 'text/html' });
-//     res.end('Welcome to my Node.js project');
-//   } else {
-//     res.writeHead(404, { 'Content-Type': 'text/html' });
-//     res.end('Page not found');
-//   }
-// });
-
-// server.listen(3000);
-
-
-
-//const http = require('http');
-// const server = http.createServer(app);
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
+app.use('/admin', adminRoutes);    // "/admin" give common to outsource so that not repated in admin.js
+app.use('/shop',shopRoutes);    //same as admin path do ^
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>');
+app.use((req, res, next) => {
+    res.status(404).send('<html>Page Not Found!</html>');
 });
 
 app.listen(3000);
